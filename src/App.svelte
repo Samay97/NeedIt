f88b0e167ff7d60dec2af9ae15a23c3f62249795
Samuel Knoch <samuel@korntal.eu>
2025-03-23 18:55:35 +0100

    fix lint

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,8 @@
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const analytics = getAnalytics(app);
+    console.log(app.name);
+    console.log(analytics.app.name);
 
     const routes: RouteConfig[] = [
         {

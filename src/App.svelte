115a1faee104144cc54785efb00a967a27e39b5c
Samuel Knoch <samuel@korntal.eu>
2025-03-23 17:48:41 +0100

    Add ui components

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,19 @@
 <script lang="ts">
-    import svelteLogo from "./assets/svelte.svg";
-    import viteLogo from "/vite.svg";
-    import Counter from "./lib/Counter.svelte";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import Moon from "@lucide/svelte/icons/moon";
+    import Sun from "@lucide/svelte/icons/sun";
+    import { ModeWatcher, toggleMode } from "mode-watcher";
 </script>
 
-<main>
-    <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
-    <h1>Hello Tauri</h1>
-
-    <div class="card">
-        <Counter />
-    </div>
-
-    <p>
-        Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-            >SvelteKit</a
-        >, the official Svelte app framework powered by Vite!
-    </p>
-
-    <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
-
-<style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-    }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-        color: #888;
-    }
-</style>
+<ModeWatcher />
+<div class="p-4">
+    <Button onclick={toggleMode} variant="outline" size="icon">
+        <Sun
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Moon
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+    </Button>
+</div>
